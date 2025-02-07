c08534d6a0a6bef79f4941d74cd45ab077f7475b
StefanoBelli <stefano9913@gmail.com>
2025-02-07 16:11:08 +0100

    market_read ok

--- src/matrix/market-read.c
+++ src/matrix/market-read.c
@@ -27,19 +27,43 @@ static int initialize_mmio_read(MM_typecode *typecode, FILE* f, uint64_t *m, uin
     return 0;
 }
 
-static inline uint64_t symmetry_fixup(struct matrix_nonzero *m, uint64_t index, uint64_t orig_nz) {
-    uint64_t diagonal_nonzeroes = 0;
-
+static inline void symmetry_fixup(struct matrix_nonzero *m, uint64_t index, uint64_t orig_nz, uint64_t *dnz, uint64_t *ez) {
     if (m[index].i == m[index].j) {
-        diagonal_nonzeroes += 1;
+        if(m[index].val != 0) {
+            *dnz += 1;
+        } else {
+            *ez += 1;
+        }
     } else {
         uint64_t i_nz_shift = index + orig_nz;
         m[i_nz_shift].i = m[index].j;
         m[i_nz_shift].j = m[index].i;
         m[i_nz_shift].val = m[index].val;
+        if(m[i_nz_shift].val == 0) {
+            *ez += 1;
+        }
     }
+}
 
-    return diagonal_nonzeroes;
+static int coo_sparse_comparator(const void *m1, const void *m2) {
+    struct matrix_nonzero *mtx1 = (struct matrix_nonzero*) m1;
+    struct matrix_nonzero *mtx2 = (struct matrix_nonzero*) m2;
+
+    if(mtx1->val == 0 && mtx2->val == 0) {
+        return 0;
+    } else if(mtx1->val == 0 && mtx2->val != 0) {
+        return 1;
+    } else if(mtx1->val != 0 && mtx2->val == 0) {
+        return -1;
+    } else {
+        if(mtx1->i > mtx2->i) {
+            return 1;
+        } else if(mtx1->i < mtx2->j) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 }
 
 struct matrix_nonzero *read_matrix_market(FILE* fp, uint64_t *m, uint64_t *n, uint64_t *nz) {
@@ -61,6 +85,8 @@ struct matrix_nonzero *read_matrix_market(FILE* fp, uint64_t *m, uint64_t *n, ui
     struct matrix_nonzero *mtx = checked_calloc(struct matrix_nonzero, initial_alloc_size);
     const char *fmt = mm_is_real(matcode) ? "%d %d %lg\n" : "%d %d\n";
 
+    uint64_t ez = 0;
+
     for (uint64_t i_nz = 0; i_nz < tmp_nz; i_nz++) {
 
 #pragma GCC diagnostic push
@@ -72,24 +98,31 @@ struct matrix_nonzero *read_matrix_market(FILE* fp, uint64_t *m, uint64_t *n, ui
             mtx[i_nz].val = 1;
         } else {
             if(mtx[i_nz].val == 0) {
-                //printf("zero read, tmp_nz = %d\n", tmp_nz);
-                //exit(EXIT_FAILURE);
+                ez++;
             }
         }
 
         if (mm_is_symmetric(matcode)) {
-            diag_nz += symmetry_fixup(mtx, i_nz, tmp_nz);
+            symmetry_fixup(mtx, i_nz, tmp_nz, &diag_nz, &ez);
         }
     }
 
     if(mm_is_symmetric(matcode)) {
         *nz = (tmp_nz * 2) - diag_nz;
-        mtx = checked_realloc(mtx, struct matrix_nonzero, *nz);
     } else {
         *nz = tmp_nz;
     }
 
-    //qsorting here
+    *nz -= ez;
+
+    qsort(mtx, initial_alloc_size, sizeof(struct matrix_nonzero), coo_sparse_comparator);
+
+    if(*nz < initial_alloc_size) {
+        mtx = checked_realloc(mtx, struct matrix_nonzero, *nz);
+    } else if (*nz > initial_alloc_size) {
+        printf("there is a bug: unexpected cond\n\t*nz > initial_alloc_size\nterminating...\n");
+        exit(EXIT_FAILURE);
+    }
 
     return mtx;
 }
