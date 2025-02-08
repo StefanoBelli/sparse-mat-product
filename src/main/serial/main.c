3938a319ee61b815e437a8cd9f70bcca6a6f2ca5
StefanoBelli <stefano9913@gmail.com>
2025-02-08 20:20:39 +0100

    wellwellwell

--- src/main/serial/main.c
+++ src/main/serial/main.c
@@ -8,24 +8,28 @@
         coo[_idx].v = _v; \
     } while(0)
 
-void ellprod(char* y, int m, const struct ellpack_repr* ell, const double* x) {
-    for(int i = 0; i < m; i++) {
+void ellprod(double* y, uint64_t m, const struct ellpack_repr* ell, const double* x) {
+    for(uint64_t i = 0; i < m; i++) {
         double t = 0;
-        for(int j = 0; j < ell->maxnz; j++) {
+        for(uint64_t j = 0; j < ell->maxnz; j++) {
             t += ell->as[i][j] * x[ell->ja[i][j]];
         }
         y[i] = t;
     }
 }
 
-int main() {
+#include<stdlib.h>
 
-    int hs = 3;
-    double vek[] = {1,0,1,0};
+int main(int ac, char** argv) {
+
+    //printf("cmdline = %s %s %s %s %s\n", argv[1], argv[2], argv[3], argv[4], argv[5]);
+
+    uint64_t hs = atoi(argv[1]);
+    double vek[] = {atoi(argv[2]),atoi(argv[3]),atoi(argv[4]),atoi(argv[5])};
     double y[] = {0,0,0,0};
 
-    int M = 4;
-    int nz = 7;
+    uint64_t M = 4;
+    uint64_t nz = 7;
     struct coo_repr coo[7];
 
     ASSIGN_COO(0, 0, 11, 0);
@@ -40,18 +44,19 @@ int main() {
 
     coo_to_csr(&csr, coo, nz, M);
 
-    for(int i = 0; i < M; i++) {
+    printf(" csr ");
+    for(uint64_t i = 0; i < M; i++) {
         double t = 0;
 
-        for(int j = csr.irp[i]; j < csr.irp[i + 1]; j++) {
+        for(uint64_t j = csr.irp[i]; j < csr.irp[i + 1]; j++) {
             t += csr.as[j] * vek[csr.ja[j]];
         }
 
         y[i] = t;
     }
 
-    for(int i = 0; i < 4; i++) {
-        printf(" %lg,", y[i]);
+    for(uint64_t i = 0; i < 4; i++) {
+        printf("%lg,", y[i]);
     }
     printf("\n");
 
@@ -65,23 +70,24 @@ int main() {
     struct hll_repr hll;
     coo_to_hll(&hll, coo, nz, M, hs);
 
-    for(int numblk = 0; numblk < hll.numblks; numblk++) {
-        char tmp[4];
+    for(uint64_t numblk = 0; numblk < hll.numblks; numblk++) {
+        double tmp[4];
         ellprod(tmp, hs, &hll.blks[numblk], vek);
-        int endrow = hs;
+        uint64_t endrow = hs;
         if (numblk * hs + hs > M) {
             endrow = M - hs;
         }
 
-        for(int i = 0; i < endrow; i++) {
+        for(uint64_t i = 0; i < endrow; i++) {
             y[i + numblk * hs] = tmp[i];
         }
     }
 
     free_hll_repr(&hll, hs);
 
-    for(int i = 0; i < 4; i++) {
-        printf(" %lg,", y[i]);
+    printf(" hll ");
+    for(uint64_t i = 0; i < 4; i++) {
+        printf("%lg,", y[i]);
     }
     printf("\n");
     return 0;
